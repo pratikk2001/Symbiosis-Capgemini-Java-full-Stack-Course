7e4f9512e27fdab336aab52a4cdf023f0a7628c8
Pratik Kankarej <pratikkankarej18@gmail.com>
2024-07-09 10:12:10 -0400

    new file

--- Java_Practice/array2.java
+++ Java_Practice/array2.java
@@ -4,23 +4,26 @@ public class array2{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		
-		int arr[]= new int[10];
+		System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+		
+		int arr[]= new int[n];
 		int count=0;
 
-		for(int i=0; i<=4;i++){
+		for(int i=0; i<n;i++){
 			System.out.print("Enter the array element :");
 			arr[i]=sc.nextInt();	
 		}
 		
 		
-		for(int i=0; i<=4;i++){
+		for(int i=0; i<n;i++){
 			System.out.println(">>>>   "+arr[i]);
 		}
 		
 		System.out.println("Enter your Search :");
 		int search = sc.nextInt();
 		
-		for(int i=0; i<=4;i++){
+		for(int i=0; i<n;i++){
 			
 			if(arr[i]==search){
 				count++;
