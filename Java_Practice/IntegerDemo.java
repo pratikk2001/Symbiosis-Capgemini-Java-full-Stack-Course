458e0f2c95b5e84c283e7a95b9838a250d30f78b
Pratik Kankarej <pratikkankarej18@gmail.com>
2024-07-13 10:15:47 -0400

    13/7/24

---
+++ Java_Practice/IntegerDemo.java
@@ -0,0 +1,12 @@
+class IntegerDemo {
+    public static void main(String[] args) {
+        Float num = 12.56f;
+        Float numb = 22.89f;
+        
+        // Converting Float to long
+        long myVal = num.longValue();
+		long myVala = numb.longValue();		// Use num or numb as needed
+        System.out.println(myVal);
+		System.out.println(myVala);
+    }
+}
---
+++ Java_Practice/IntegerDemo1.java
@@ -0,0 +1,12 @@
+class IntegerDemo1 {
+    public static void main(String[] args) {
+        Integer num = 21122;
+        Integer numb = 43222;
+        
+        // Converting Int to long
+        Long myVal = num.longValue();
+		Long myVala = numb.longValue();		// Use num or numb as needed
+        System.out.println(myVal);
+		System.out.println(myVala);
+    }
+}
