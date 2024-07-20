dfd0ca1cb846892ad741aced860c5fdd47c67c4d
Pratik Kankarej <pratikkankarej18@gmail.com>
2024-07-19 22:22:48 -0400

    new

---
+++ Java_Practice/text.java
@@ -0,0 +1,20 @@
+class xyz{
+	{
+	void disp(int x)
+	system.out.println("good")
+	}
+	void disp(String y){
+	{
+	System.out.println("Bye");
+	}
+}
+}
+
+
+public class main{
+	public static void main (String args[]){
+		xyz  set = new xyz();
+		System.out.println("Good");
+		set.disp(null);
+	}
+}
---
+++ Java_Practice/xyz.java
@@ -0,0 +1,9 @@
+
+
+public class main{
+	public static void main (String args[]){
+		int x = 'y';
+		System.out.println(y);
+		
+	}
+}
