93e9b699a5ad8661c1ad9f7ceb119e472fcefca5
Pratik Kankarej <pratikkankarej18@gmail.com>
2024-07-21 21:43:57 -0400

    22-07-24

---
+++ Java_Practice/Que1.java
@@ -0,0 +1,10 @@
+public class Que1{
+	static int i =10;
+	public static void main(String[] args){
+		changeValue(i);
+		System.out.println(i);
+		}
+		public static void changeValue(int i){
+			i= 50;
+			}
+			}
